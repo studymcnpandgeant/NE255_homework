3cc05abc3c2975fb2a0c03dc84e613ffa5a388fe
Xin Wang <imwangxin@gmail.com>
2014-11-18 17:55:10 -0800

    added HW4

--- HW4_MC/MC_slab.m
+++ HW4_MC/MC_slab.m
@@ -20,15 +20,25 @@ z_s_max = 4.0;
 mesh_node_nb = 60;
 h = (z_max - z_min)/(mesh_node_nb -1);
 z_mesh = linspace(z_min, z_max-h, mesh_node_nb-1);
-col_nb = zeros(mesh_node_nb-1, 1);
+
 
 %source neutrons number
 n_array = [1000, 8000, 16000 , 32000];
+%n_array = [125, 250, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000];
 
-for i =1:4
+    
+for i =1:length(n_array)
     n = n_array(i);
     N_L =0; N_R =0; N_AB =0;  N =0;
-    N_AB_L= 0; N_AB_R = 0; 
+    N_AB_L= 0; N_AB_R = 0; N_col =0;
+    col_nb = zeros(mesh_node_nb-1, 1);
+    p_L_arr = zeros(length(n_array),1);
+    p_R_arr = zeros(length(n_array),1);
+    %prob of absorption
+    p_AB_L_arr = zeros(length(n_array),1);
+    p_AB_R_arr = zeros(length(n_array),1);
+    k=1;
+
     while N < n
         N=N+1;
         %sample a new particle
@@ -46,15 +56,16 @@ for i =1:4
                 %---leackage at the right end
                 N_R = N_R +1;
                 despair = true;
-                break
-            else
+            end
             if z < z_min
                 %--leackage at the left end
                 N_L = N_L+1;
                 despair =true;
-                break
-            else
-                col_nb(ceil((z-z_min)/h)) = col_nb(ceil((z-z_min)/h)) +1;
+            end
+            if z>z_min && z< z_max
+                N_col = N_col +1;
+                
+               col_nb(ceil((z-z_min)/h)) = col_nb(ceil(((z-z_min)/h))) +1;
                 if rand > Pc
                     %---absorption
                     N_AB = N_AB +1;
@@ -70,12 +81,18 @@ for i =1:4
                         mu = ( -1 + sqrt(1-3 * mu * mu_0_bar * ( 2*(1-2*rand) - 3* mu * mu_0_bar) ) )/3/mu/mu_0_bar;
                 end
             end
-            end
+            
 
-        end        
-
+        end            
+    
     end
-
+    %prob of escaping
+    p_L_arr(k) = N_L/n;
+    p_R_arr(k) = N_R/n;
+    %prob of absorption
+    p_AB_L_arr(k) = N_AB_L/n;
+    p_AB_R_arr(k) = N_AB_R/n;
+    k=k+1;
 
     %current 
     J_L = N_L/n * 4;
@@ -95,7 +112,8 @@ for i =1:4
     %flux from collision rate
     flux = 4*col_nb/sigma_t/h/n;
     
-
+   display(sum(col_nb));
+   
     stairs(z_mesh, flux);
     hold all on;
 end
--- HW4_MC/MC_slab_photon.m
+++ HW4_MC/MC_slab_photon.m
@@ -5,7 +5,7 @@ clc;
 
 %attenuation coef for photon in concrete 
 rho = 2.3; %g/cm3
-mu_tot = 4.557 * 0.01 * rho; %cm
+Sigma_tot = 4.557 * 0.01 * rho; %cm
 
 Av = 6.02 *10^23; %#/mol
 Z_concrete = 1*0.022100 + 6*0.002484 + 8*0.574930 + 11 * 0.015208 +...
@@ -16,79 +16,95 @@ Ein = 2; %Mev
 m0c2 =0.511; %Mev
 r0 = 2.8179403267 * 10^(-13);%cm classical electron radius
 alpha = Ein/m0c2;
-sigma_cs = 2*pi*r0^2*( (1+alpha)/alpha^2 * ( 2*(1+alpha)/(1+2*alpha) - 1/alpha*log(1+2*alpha) ) + (1/2/alpha*log(1+2*alpha)...
+mu_cs = 2*pi*r0^2*( (1+alpha)/alpha^2 * ( 2*(1+alpha)/(1+2*alpha) - 1/alpha*log(1+2*alpha) ) + (1/2/alpha*log(1+2*alpha)...
     -(1+3*alpha)/(1+2*alpha)^2) );%cm^2*electron-1
-mu_cs = sigma_cs * N_con ;
+Sigma_cs = mu_cs * N_con ;
 
-P_cs = mu_cs/mu_tot;
-
+P_cs = Sigma_cs/Sigma_tot;
 
 %slab length
 z_min =0;
 z_max = 5;%cm
 
-%incident point
-z = 0;
-%incident direction: normal to the surface
-mu = 1;
+N_ref =0;
+N_source = 60000;
+N_AB=0;
+N = 0;
+N_L=0;
+E_ref =0;
+while N < N_source
+    N=N+1;
+    %incident point
+    z = 0;
+    %incident direction: normal to the surface
+    mu_in = 1;
+    %calculate the first interaction point x1
+    dist = -mu_in * log(rand)/Sigma_tot;
+    z = z + dist;
 
-%calculate the first interaction point x1
-dist = -mu * log(rand)/mu_tot;
-z = z + dist;
+    if z < z_max && z > z_min
+        %sample the collision type
+        if rand < P_cs
+            %sample the scattering out energy
+            lambda_in = m0c2/Ein;
+            %K=3/16*(9*lambda_in -2)/(lambda_in +2);
+            a1 = (lambda_in+2)/(9*lambda_in +2);
 
-nb =0;
-
-N = 30000;
-n = 0;
-while n < N
-    
-if z < z_max && z > z_min
-    %sample the collision type
-    if rand < P_cs
-        %sample the scattering out energy
-        lambda_in = m0c2/Ein;
-        %K=3/16*(9*lambda_in -2)/(lambda_in +2);
-        a1 = (lambda_in+2)/(9*lambda_in +2);
-        sampled = false;
-        while sampled == false
-            if rand <= a1
-                x1 = 1+2*rand/lambda_in;%g1 = lambda_in/2;
-                h1 =@(y) 4*(1/y - 1/y^2);
-                if rand <= h1(x1)
-                    lambda_out = x1*lambda_in;
-                    E_out = m0c2/lambda_out;
-                    sampled = true;
-                end
-            else
-                x1 = (lambda_in +2)/(lambda_in +2*rand);%g2 = (lambda_in +2)/2/y^2;
-                h2 =@(y) 1/2*((lambda_in - lambda_in *y +1)^2 +1/y)^2;
-                if rand <= h2(x1)
-                    lambda_out = x1*lambda_in;
-                    E_out = m0c2/lambda_out;
-                    sampled = true;
+            sampled = false;
+            while sampled == false
+                if rand <= a1
+                    x1 = 1+2*rand/lambda_in;%g1 = lambda_in/2;
+                    h1 =@(y) 4*(1/y - 1/y^2);
+                    if rand <= h1(x1)
+                        lambda_out = x1*lambda_in;
+                        E_out = m0c2/lambda_out;
+                        sampled = true;
+                    end
+                else
+                    x1 = (lambda_in +2)/(lambda_in +2*rand);%g2 = (lambda_in +2)/2/y^2;
+                    h2 =@(y) 1/2*((lambda_in - lambda_in *y +1)^2 +1/y)^2;
+                    if rand <= h2(x1)
+                        lambda_out = x1*lambda_in;
+                        E_out = m0c2/lambda_out;
+                        sampled = true;
+                    end
                 end
             end
-        end
-        cos_theta_s = 1- lambda_out +lambda_in;
-        beta = 2*pi*rand;
-        mu_out = mu * cos_theta_s + sqrt(1-mu^2) * sqrt(1-cos_theta_s^2) *cos(beta);%??
-        display(mu_out);
-        if mu_out <0
-            dist = -mu_out * log(rand)/mu_tot;
-            if dist >= abs(z/cos(mu_out))
-                nb = nb+1;
+            cos_theta_s = 1- lambda_out +lambda_in;
+            beta = 2*pi*rand;
+            if rand<0.5
+            n=1;
+            else n=2;
+            end
+            if rand <0.5
+                m=1;
+            else m=2;
+            end
+            
+            mu_out = mu_in * cos_theta_s + (-1)^n*sqrt(1-mu_in^2) * (-1)^m*sqrt(1-cos_theta_s^2) *cos(beta);
+                
+            %mu_out = mu_in/cos_theta_s;
+            display(mu_out);
+            if mu_out <0
+                N_L=N_L+1;
+                dist =abs( mu_out * log(rand)/Sigma_tot);
+                display(dist);
+                if dist >= abs(z/mu_out)
+                    N_ref = N_ref+1;
+                    E_ref = E_ref + abs(E_out/mu_out);
+                end
+                
             end
         end
+    else  %absorption
+        N_AB = N_AB +1;
     end
-    
-    end
-    
-  n = n+1;      
+
 end
 
         
-albedo = nb/N;
-        
+albedo = N_ref/N_source;
+albedo_E = E_ref/N_source;
         
        
 
